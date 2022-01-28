0559d0850adcd0e376aacf84879c6e59761023c6
daking24 <daking3968@outlook.com>
2022-01-28 14:06:55 +0100

    project backend

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ajayi king-david o\desktop\projects\django\django-3-by-example-master\chapter09\myshop\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
